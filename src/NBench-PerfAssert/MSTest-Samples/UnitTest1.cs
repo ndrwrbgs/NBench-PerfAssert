4fa5bf753504af1ab44d088a877be3c1f3e717f6
ndrwrbgs <ndrwrbgs@users.noreply.github.com>
2019-09-10 16:55:15 -0700

    Andrew needs to learn to NOT push before running the code

--- src/NBench-PerfAssert/MSTest-Samples/UnitTest1.cs
+++ src/NBench-PerfAssert/MSTest-Samples/UnitTest1.cs
@@ -25,12 +25,14 @@ namespace MSTest_Samples
         }
 
         [PerfBenchmark /* TODO: We would like that this only requires a type from our library, but permit types from NBench for ease */]
+        [ElapsedTimeAssertion]
         public /* TODO: We would like to hide these methods, but currently required exposed by NBench */ void Faster()
         {
             Thread.Sleep(10);
         }
 
         [PerfBenchmark]
+        [ElapsedTimeAssertion]
         public void Slower()
         {
             Thread.Sleep(100);
